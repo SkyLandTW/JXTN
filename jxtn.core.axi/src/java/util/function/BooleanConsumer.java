4f732cf0f2929a511330ecad1de783ae63f8fa5a
AqD <iiiaqd@gmail.com>
2014-12-06 23:59:00 +0800

    add BooleanConsumer / BooleanConsumerEx

---
+++ jxtn.core.axi/src/java/util/function/BooleanConsumer.java
@@ -0,0 +1,63 @@
+/*
+ * This is free and unencumbered software released into the public domain.
+ *
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
+ *
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * For more information, please refer to <http://unlicense.org/>
+ */
+
+package java.util.function;
+
+import java.util.Objects;
+
+/**
+ * 接收單一布林值的動作
+ *
+ * @author AqD
+ */
+@FunctionalInterface
+public interface BooleanConsumer
+{
+    /**
+     * 對布林值進行操作
+     *
+     * @param value 布林值
+     */
+    void accept(boolean value);
+
+    /**
+     * 傳回合併目前操作及指定操作的新{@link BooleanConsumer}，依序執行。
+     *
+     * @param after 執行完目前操作後要再進行的操作
+     * @return 合併目前操作及指定操作的新{@link BooleanConsumer}
+     * @throws NullPointerException {@code after}為null
+     */
+    default BooleanConsumer andThen(BooleanConsumer after)
+    {
+        Objects.requireNonNull(after);
+        return (boolean t) ->
+            {
+                accept(t);
+                after.accept(t);
+            };
+    }
+}

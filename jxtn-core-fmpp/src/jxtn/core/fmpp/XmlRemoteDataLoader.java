d18a591c328f3c763585a44db060a49fb7d27226
Ji-Ping Shen <iiiaqd@gmail.com>
2018-11-06 12:14:19 +0800

    turn off HTTPS/SSL validation in fmpp XmlRemoteDataLoader

--- jxtn-core-fmpp/src/jxtn/core/fmpp/XmlRemoteDataLoader.java
+++ jxtn-core-fmpp/src/jxtn/core/fmpp/XmlRemoteDataLoader.java
@@ -27,16 +27,22 @@
 
 package jxtn.core.fmpp;
 
-import java.net.URL;
-import java.util.List;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
-import org.w3c.dom.Document;
-
 import fmpp.Engine;
 import fmpp.dataloaders.XmlDataLoader;
+import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+import java.util.List;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
 
 /**
  * 以JAR檔類別結構作為資料來源的載入器
@@ -48,6 +54,47 @@ import fmpp.dataloaders.XmlDataLoader;
  */
 @SuppressWarnings({ "rawtypes" })
 public class XmlRemoteDataLoader extends XmlDataLoader {
+
+    static {
+        // from http://www.nakov.com/blog/2009/07/16/disable-certificate-validation-in-java-ssl-connections/
+        // Create a trust manager that does not validate certificate chains
+        TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+            @Override
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                return null;
+            }
+
+            @Override
+            public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                //
+            }
+
+            @Override
+            public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                //
+            }
+        } };
+        // Install the all-trusting trust manager
+        SSLContext sc;
+        try {
+            sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        // Create all-trusting host name verifier
+        HostnameVerifier allHostsValid = new HostnameVerifier() {
+            @Override
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        };
+        // Install the all-trusting host verifier
+        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+    }
 
     @Override
     public Object load(Engine engine, List args) throws Exception {
